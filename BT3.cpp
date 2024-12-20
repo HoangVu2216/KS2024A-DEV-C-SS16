cb7262a3d7fb27c78a6638f3ab4168c3f8babecd
HoangVu2216 <vutrananhhoang2216@gmail.com>
2024-12-20 09:48:52 +0700

    Add files via upload
    BÀI TẬP CƠ BẢN

---
+++ BT3.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void tinhTong(int a, int b, int *ketQua) {
+    *ketQua = a + b;
+}
+
+int main() {
+    int x = 7, y = 13, ketQua;
+    tinhTong(x, y, &ketQua);
+    printf("Tong cua %d va %d la: %d\n", x, y, ketQua);
+    return 0;
+}
+
